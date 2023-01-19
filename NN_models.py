correct config mask calculation in train_reconfigurable.py

NN_models.py
@@ -6,9 +6,6 @@ import time
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-import torch.multiprocessing as mp
-
-mp = mp.get_context('spawn')
 
 
 # HybridNet predict both grasping quality and grasping configuration
@@ -19,16 +16,18 @@ class HybridNet(nn.Module):
         self.use_cuda = use_cuda
 
         # Initialize network trunks with DenseNet pre-trained on ImageNet
-        self.grasp_color_trunk = torchvision.models.densenet.densenet121(pretrained=True)
-        self.grasp_depth_trunk = torchvision.models.densenet.densenet121(pretrained=True)
+        self.grasp_color_trunk = torchvision.models.densenet.densenet161(pretrained=False)
+        self.grasp_depth_trunk = torchvision.models.densenet.densenet161(pretrained=False)
+
+        self.feature_dim = 4416
 
         self.num_rotations = 16
 
         # Construct network branches for grasping (Quality Map)
         self.graspnet = nn.Sequential(OrderedDict([
-            ('grasp-norm0', nn.BatchNorm2d(2048)),
+            ('grasp-norm0', nn.BatchNorm2d(self.feature_dim)),
             ('grasp-relu0', nn.ReLU(inplace=True)),
-            ('grasp-conv0', nn.Conv2d(2048, 64, kernel_size=1, stride=1, bias=False)),
+            ('grasp-conv0', nn.Conv2d(self.feature_dim, 64, kernel_size=1, stride=1, bias=False)),
             ('grasp-norm1', nn.BatchNorm2d(64)),
             ('grasp-relu1', nn.ReLU(inplace=True)),
             ('grasp-conv1', nn.Conv2d(64, 1, kernel_size=1, stride=1, bias=False))
@@ -36,9 +35,9 @@ class HybridNet(nn.Module):
 
         # Construct network branches for grasping (Configuration Map)
         self.confignet = nn.Sequential(OrderedDict([
-            ('config-norm0', nn.BatchNorm2d(2048)),
+            ('config-norm0', nn.BatchNorm2d(self.feature_dim)),
             ('config-relu0', nn.ReLU(inplace=True)),
-            ('config-conv0', nn.Conv2d(2048, 64, kernel_size=1, stride=1, bias=False)),
+            ('config-conv0', nn.Conv2d(self.feature_dim, 64, kernel_size=1, stride=1, bias=False)),
             ('config-norm1', nn.BatchNorm2d(64)),
             ('config-relu1', nn.ReLU(inplace=True)),
             ('config-conv1', nn.Conv2d(64, 1, kernel_size=1, stride=1, bias=False)),

train_reconfigurable.py
@@ -11,6 +11,7 @@ from logger import Logger
 import utils
 import threading
 import warnings
+
 warnings.filterwarnings('ignore')  # CAREFULL!!! Supress all warnings
 
 
@@ -109,11 +110,11 @@ def main(args):
                 # Visualize executed primitive, and affordances
                 if save_visualizations:
                     config_pred_vis = get_prediction_vis(config_predictions, color_heightmap,
-                                                                 nonlocal_variables['best_pix_ind'])
+                                                         nonlocal_variables['best_pix_ind'])
                     logger.save_visualizations(trainer.iteration, config_pred_vis, 'config')
                     cv2.imwrite('visualization.config.png', config_pred_vis)
                     grasp_pred_vis = get_prediction_vis(grasp_predictions, color_heightmap,
-                                                                nonlocal_variables['best_pix_ind'])
+                                                        nonlocal_variables['best_pix_ind'])
                     logger.save_visualizations(trainer.iteration, grasp_pred_vis, 'grasp')
                     cv2.imwrite('visualization.grasp.png', grasp_pred_vis)
 
@@ -123,7 +124,7 @@ def main(args):
                 if primitive_position[2] < 0.2:
                     # Execute primitive
                     nonlocal_variables['grasp_success'] = robot.grasp(primitive_position, best_rotation_angle,
-                                                                      (np.pi / 2 * primitive_config) - np.pi / 2,
+                                                                      (np.pi / 3 * primitive_config) - np.pi / 2,
                                                                       workspace_limits)
                     print('Grasp successful: %r' % (nonlocal_variables['grasp_success']))
 
@@ -223,8 +224,12 @@ def main(args):
             logger.write_to_log('reward-value', trainer.reward_value_log)
 
             # Backpropagate
-            trainer.backprop(prev_color_heightmap, prev_valid_depth_heightmap, prev_best_pix_ind,
-                             label_value, prev_best_config)
+            loss_q_value, loss_config_value = trainer.backprop(prev_color_heightmap, prev_valid_depth_heightmap,
+                                                               prev_best_pix_ind,
+                                                               label_value, prev_best_config)
+
+            logger.writer.add_scalar('q_loss', loss_q_value, trainer.iteration)
+            logger.writer.add_scalar('config_loss', loss_config_value, trainer.iteration)
 
             # Save model snapshot
             logger.save_backup_model(trainer.model, 'reconfig')
@@ -253,6 +258,7 @@ def main(args):
         trainer.iteration += 1
         iteration_time_1 = time.time()
         print('Time elapsed: %f' % (iteration_time_1 - iteration_time_0))
+
 
 if __name__ == '__main__':
     # Parse arguments

trainer.py
@@ -251,12 +251,11 @@ class HybridTrainer(Trainer):
         # Compute config mask
         config_mask = np.zeros((1, 320, 320))
         tmp_config_mask = np.zeros((224, 224))
-        tmp_label[config_area > 0] = 1
+        tmp_config_mask[config_area > 0] = 1
         config_mask[0, 48:(320 - 48), 48:(320 - 48)] = tmp_config_mask
 
         # Compute loss and backward pass
         self.optimizer.zero_grad()
-        loss_value = 0
 
         if label_value > 0:  # When successful, compute both quality and config loss
 
@@ -282,9 +281,10 @@ class HybridTrainer(Trainer):
                     torch.from_numpy(label_weights).float(), requires_grad=False)
             loss = loss_q.sum() + loss_config.sum()
             loss.backward()
-            loss_value = loss.cpu().data.numpy()
+            loss_q_value = loss_q.sum().cpu().data.numpy()
+            loss_config_value = loss_config.sum().cpu().data.numpy()
 
-            print('Training loss: %f' % loss_value)
+            print('Q loss: %f;' % loss_q_value, 'config loss: %f;' % loss_config_value)
             self.optimizer.step()
 
         else:  # Only implement backpropagation on q net
@@ -303,21 +303,27 @@ class HybridTrainer(Trainer):
                     torch.from_numpy(label_weights).float(), requires_grad=False)
             loss = loss_q.sum()
             loss.backward()
-            loss_value = loss.cpu().data.numpy()
-
-            print('Training loss: %f' % loss_value)
+            loss_q_value = loss_q.sum().cpu().data.numpy()
+            loss_config_value = 0
+            print('Q loss: %f' % loss_q_value)
             self.optimizer.step()
-            return loss_value
+        return loss_q_value, loss_config_value
 
 
 class MultiQTrainer(Trainer):
-    def __init__(self, future_reward_discount, load_snapshot, snapshot_file, force_cpu):  # , snapshot=None
+    def __init__(self, network_type, future_reward_discount, load_snapshot, snapshot_file, force_cpu):  # , snapshot=None
         super(MultiQTrainer, self).__init__(force_cpu)
 
         self.grasp_mode_count = [0, 0]
 
         # Fully convolutional network
-        self.model = TeacherNet(use_cuda=self.use_cuda)
+        if network_type == 'teacher':
+            self.model = TeacherNet(use_cuda=self.use_cuda)
+        elif network_type == 'student':
+            self.model = StudentNet(use_cuda=self.use_cuda)
+        else:
+            raise ValueError("Network type error")
+
         self.future_reward_discount = future_reward_discount
 
         # Load pre-trained model